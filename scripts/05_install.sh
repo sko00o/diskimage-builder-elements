e797fc305bd3ae1ad3cca29d55c5bf8405e35fb9
sko00o <lwylwy@outlook.com>
2023-08-17 16:44:28 +0800

    setup mirror

--- scripts/05_install.sh
+++ scripts/05_install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -exo pipefail
 
+CUDA_REPO="https://mirrors.aliyun.com/nvidia-cuda"
+MINICONDA_REPO="https://mirrors.aliyun.com/anaconda/miniconda"
+
 cd "$(dirname "$0")"
 for helper in ./helpers/*.sh; do
     source "$helper"
 done
+
+setup_pip_mirror
+setup_conda_mirror
 
 install_cuda
 install_node_exporter
