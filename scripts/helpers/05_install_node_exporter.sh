70506070f04cdabdf5fcc7177d8e1d773b1de862
sko00o <lwylwy@outlook.com>
2023-08-17 13:05:30 +0800

    update setup_node_exporter_binary

--- scripts/helpers/05_install_node_exporter.sh
+++ scripts/helpers/05_install_node_exporter.sh
@@ -7,23 +7,24 @@ setup_node_exporter_binary() {
     # Check if Node Exporter binary exists
     if command -v node_exporter &>/dev/null; then
         echo "Node Exporter binary already exists."
-    else
-        # Download Node Exporter
-        wget ${NODE_EXPORTER_URL}
-
-        # Extract Node Exporter
-        tar -xzvf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
-
-        # Move Node Exporter binary
-        sudo mv node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter /usr/local/bin
-
-        # Cleanup
-        rm -rf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64
+        return
     fi
+
+    # Download Node Exporter
+    wget ${NODE_EXPORTER_URL}
+
+    # Extract Node Exporter
+    tar -xzvf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz
+
+    # Move Node Exporter binary
+    sudo mv node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64/node_exporter /usr/local/bin
+
+    # Cleanup
+    rm -rf node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64.tar.gz node_exporter-${NODE_EXPORTER_VERSION}.linux-amd64
 }
 
 setup_node_exporter_systemd() {
-    # Check if Node Exporter is already installed
+    # Check if Node Exporter systemd service is active
     if systemctl is-active --quiet node_exporter.service; then
         echo "Node Exporter is already installed and active."
         return
