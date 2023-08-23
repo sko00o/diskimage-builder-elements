a61607b760b1e40ee74f19e1a013e102ad1d2b18
sko00o <lwylwy@outlook.com>
2023-08-23 23:34:15 +0800

    use install-static

--- scripts-install/scripts/00_prepare.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -exo pipefail
-
-# Update package lists
-sudo apt-get update
-
-# Install necessary dependencies
-sudo apt-get install -y build-essential apt-transport-https ca-certificates curl software-properties-common
