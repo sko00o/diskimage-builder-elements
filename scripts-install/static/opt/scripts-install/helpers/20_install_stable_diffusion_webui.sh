bfb4de3dde8b836cc12d123c97a582e36d8d3e01
sko00o <lwylwy@outlook.com>
2023-09-04 10:46:32 +0800

    cleanup

--- scripts-install/static/opt/scripts-install/helpers/20_install_stable_diffusion_webui.sh
+++ scripts-install/static/opt/scripts-install/helpers/20_install_stable_diffusion_webui.sh
@@ -3,13 +3,12 @@
 SD_WEBUI_TAG=${SD_WEBUI_TAG:-"v1.5.2"}
 SD_WEBUI_REPO=${SD_WEBUI_REPO:-"https://github.com/AUTOMATIC1111/stable-diffusion-webui.git"}
 SD_WEBUI_DIR=${SD_WEBUI_DIR:-"${HOME}/stable-diffusion-webui"}
+SD_WEBUI_PORT=${SD_WEBUI_PORT:-"10000"}
 
 SD_WEBUI_DATA_DIR=${SD_WEBUI_DATA_DIR:-"${SD_WEBUI_DIR}"}
 SD_WEBUI_CKPT=${SD_WEBUI_CKPT:-"${SD_WEBUI_DATA_DIR}/models/Stable-diffusion/v1-5-pruned-emaonly.safetensors"}
 SD_WEBUI_CKPT_URL=${SD_WEBUI_CKPT_URL:-"https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.safetensors"}
-SD_WEBUI_PORT=${SD_WEBUI_PORT:-"10000"}
 SD_WEBUI_EXT_DIR=${SD_WEBUI_EXT_DIR:-"${SD_WEBUI_DATA_DIR}/extensions"}
-
 ADETAILER_TAG=${ADETAILER_TAG:-"v23.8.1"}
 CONTROLNET_COMMIT=${CONTROLNET_COMMIT:-"c3b32f2"}
 
@@ -63,17 +62,6 @@ install_sd_webui() {
     install_miniconda
     conda create -y -n sd_webui python=3.10
     $(conda info --base)/envs/sd_webui/bin/python3.10 -m venv venv
-
-    # PyTorch
-    # pip install \
-    #     torch==2.0.1+cu118 \
-    #     torchvision==0.15.2+cu118 \
-    #     torchaudio==2.0.2 \
-    #     --index-url https://download.pytorch.org/whl/cu118
-    # GFPGAN
-    #pip install git+https://github.com/TencentARC/GFPGAN.git --prefer-binary
-    # CodeFormer
-    #git clone https://github.com/sczhou/CodeFormer.git repositories/CodeFormer
 
     # launch and wait, It will take long time on first boot
     launch_sd_webui
