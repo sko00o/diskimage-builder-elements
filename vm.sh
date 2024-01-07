7421858f6bba38f158804d0d78e79190f3c67268
sko00o <lwylwy@outlook.com>
2024-01-07 22:38:58 +0800

    install multipass if not installed

--- vm.sh
+++ vm.sh
@@ -3,6 +3,11 @@ set -e
 
 cd "$(dirname "$0")"
 
+# if multipass not installed, install it
+if ! command -v multipass &> /dev/null; then
+  sudo snap install multipass --classic
+fi
+
 # if imgbuild not found, create it
 if ! multipass list | grep -q imgbuild; then
   multipass launch \
@@ -13,4 +18,5 @@ if ! multipass list | grep -q imgbuild; then
     22.04
 fi
 
+# attack the imgbuild vm
 multipass exec imgbuild -- sudo -i
