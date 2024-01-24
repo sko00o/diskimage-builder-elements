01a386afac040327cbf7b4bb33fd5221ac76289c
sko00o <lwylwy@outlook.com>
2024-01-24 21:27:27 +0800

    use ubuntu 20.04 for build

--- vm.sh
+++ vm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+instance_name=imgbuild2004
+
 cd "$(dirname "$0")"
 
 # if multipass not installed, install it
@@ -9,14 +11,14 @@ if ! command -v multipass &> /dev/null; then
 fi
 
 # if imgbuild not found, create it
-if ! multipass list | grep -q imgbuild; then
+if ! multipass list | grep -q $instance_name; then
   multipass launch \
-    --name imgbuild \
+    --name $instance_name \
     --cpus 4 --memory 8G --disk 80G \
     --mount $(pwd):/root/build \
     --cloud-init cloud-init.yaml \
-    22.04
+    20.04
 fi
 
 # attack the imgbuild vm
-multipass exec imgbuild -- sudo -i
+multipass exec $instance_name -- sudo -i
