4e47a7babc1e409f3ec678d4680f81f4fa744622
sko00o <lwylwy@outlook.com>
2024-05-14 18:23:46 +0800

    open-webui: patch

--- scripts-install/pack.sh
+++ scripts-install/pack.sh
@@ -8,6 +8,7 @@ cp -av ../node-exporter/static/* static/
 cp -av ../health-check/bin/* static/usr/local/bin/
 cp -av ../systemd-jupyterlab/bin/* static/usr/local/bin/
 cp -av ../prometheus/bin/* static/usr/local/bin/
+cp -av ../open-webui/bin/* static/usr/local/bin/
 
 cp -av ../comfyui/bin/* static/usr/local/bin/
 cp -av ../rvc-webui-public-data/bin/* static/usr/local/bin/
