a17474c4c20960e03e426a845ec3733cdb983272
sko00o <lwylwy@outlook.com>
2024-08-14 16:09:05 +0800

    new comfyui image

--- scripts-install/pack.sh
+++ scripts-install/pack.sh
@@ -15,7 +15,7 @@ cp -av ../systemd-jupyterlab/bin/* static/usr/local/bin/
 cp -av ../prometheus/bin/* static/usr/local/bin/
 cp -av ../open-webui/bin/* static/usr/local/bin/
 
-cp -av ../comfyui/bin/* static/usr/local/bin/
+# cp -av ../comfyui/bin/* static/usr/local/bin/
 cp -av ../rvc-webui-public-data/bin/* static/usr/local/bin/
 # cp -av ../sd-webui-common/bin/* static/usr/local/bin/
 cp -av ../fooocus/bin/* static/usr/local/bin/
